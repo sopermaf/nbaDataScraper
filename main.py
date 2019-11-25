858bf7aed8a384c8bc6c760e6b2b624503fe84c4
ferdia <ferdia@omko.co>
2019-11-25 08:10:59 +0000

    Updated main running function to display previous night games and upcoming games if no args passed

--- main.py
+++ main.py
@@ -7,20 +7,44 @@ games as well as their record
 import json
 import sys
 from nbaObjects import NBAGameDay
+import datetime
 
+def getTonightsGames():
+    tonightsGames = NBAGameDay()
+    tonightData = datetime.datetime.now()
+    tonightDataArg = tonightData.date().isoformat()
+    tonightsGames.initDataRemote(tonightDataArg)
+    return tonightsGames
+
+def getPreviousNightGames():
+    previousNightGames = NBAGameDay()
+    yesterdayDate = datetime.datetime.now() - datetime.timedelta(days=1)
+    nbaDataArg = yesterdayDate.date().isoformat()
+    previousNightGames.initDataRemote(nbaDataArg)
+    return previousNightGames
+
+def displayYesterdayAndToday():
+    todayGames = getTonightsGames()
+    yesterGames = getPreviousNightGames()
+
+    print("***RESULTS FROM LAST NIGHT***")
+    yesterGames.print()
+    print("**UPCOMING GAMES***")
+    todayGames.print()
 
 if __name__=="__main__":
-    retrievalMethod = sys.argv[1] # -f || -r
-    retrievalRef = sys.argv[2] # filename or date YYYYMMDD
-
-    dayData = NBAGameDay()
-    if retrievalMethod == '-f':
-        dayData.initDataLocalFile(retrievalRef)
-    elif retrievalMethod == '-r':
-        dayData.initDataRemote(retrievalRef)
+    if len(sys.argv) < 2:
+        displayYesterdayAndToday()
     else:
-        raise ValueError(
-            'INVALID ARGS'
-        )
-    
-    dayData.print()
+        dayData = NBAGameDay()
+        retrievalMethod = sys.argv[1] # -f || -r
+        retrievalRef = sys.argv[2] # filename or date YYYYMMDD
+        if retrievalMethod == '-f':
+            dayData.initDataLocalFile(retrievalRef)
+        elif retrievalMethod == '-r':
+            dayData.initDataRemote(retrievalRef)
+        else:
+            raise ValueError(
+                'INVALID ARGS'
+            )
+        dayData.print()
